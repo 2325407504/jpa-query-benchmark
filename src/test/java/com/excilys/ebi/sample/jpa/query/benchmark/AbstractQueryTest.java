06a3e433d00d2c916f17102fd3a772dcb359c874
Stephane Landelle <slandelle+github@excilys.com>
2011-08-16 22:23:55 +0200

    Use spring-junit and junit-benchmarks

--- src/test/java/com/excilys/ebi/sample/jpa/query/benchmark/AbstractQueryTest.java
+++ src/test/java/com/excilys/ebi/sample/jpa/query/benchmark/AbstractQueryTest.java
@@ -1,36 +1,37 @@
 package com.excilys.ebi.sample.jpa.query.benchmark;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import javax.annotation.Resource;
 
-import org.junit.Assert;
+import junit.framework.Assert;
+
+import org.junit.Rule;
+import org.junit.rules.MethodRule;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
+import com.carrotsearch.junitbenchmarks.BenchmarkOptions;
+import com.carrotsearch.junitbenchmarks.BenchmarkRule;
 import com.excilys.ebi.sample.jpa.query.benchmark.model.Song;
 import com.excilys.ebi.sample.jpa.query.benchmark.repository.IRepository;
 import com.excilys.ebi.sample.jpa.query.benchmark.repository.ISpringDataRepository;
-import com.excilys.utils.spring.test.dbunit.DataSet;
-import com.excilys.utils.spring.test.dbunit.DataSetTestExecutionListener;
+import com.excilys.ebi.spring.dbunit.test.DataSet;
+import com.excilys.ebi.spring.dbunit.test.DataSetTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("JPARepositoryTest-context.xml")
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DataSetTestExecutionListener.class })
 @DataSet
+@BenchmarkOptions(benchmarkRounds = 1000, warmupRounds = 100)
 public class AbstractQueryTest {
+
+	@Rule
+	public MethodRule benchmarkRun = new BenchmarkRule();
 
 	@Resource(name = "JPQLRepository")
 	protected IRepository jpqlRepository;
@@ -47,41 +48,9 @@ public class AbstractQueryTest {
 	@Autowired
 	protected ISpringDataRepository springDataRepository;
 
-	protected static final int WARM_UP_ITERATIONS = 50000;
-
-	protected static final int TEST_ITERATIONS = 10000;
-
-	protected static final int THREAD_POOL_SIZE = 4;
-
-	protected ExecutorService executorService = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
-
-	protected void run(Callable<List<Song>> callable, String testName, Logger logger) throws InterruptedException, ExecutionException {
-
-		// warm up
-		invokeCallable(callable, WARM_UP_ITERATIONS);
-
-		// run
-		Long start = System.nanoTime();
-		invokeCallable(callable, TEST_ITERATIONS);
-
-		Long end = System.nanoTime();
-		logger.info("{} {}ms", testName, (end - start) / 1000000);
-	}
-
-	protected void invokeCallable(Callable<List<Song>> callable, int times) throws InterruptedException, ExecutionException {
-
-		Collection<Callable<List<Song>>> callables = new ArrayList<Callable<List<Song>>>();
-		for (int i = 0; i < times; i++) {
-			callables.add(callable);
-		}
-
-		List<Future<List<Song>>> results = executorService.invokeAll(callables);
-
-		for (Future<List<Song>> result : results) {
-			List<Song> songs = result.get();
-			Assert.assertTrue(songs.size() == 12);
-			Assert.assertEquals("Consternation", songs.get(0).getTitle());
-			Assert.assertEquals("The Itch", songs.get(11).getTitle());
-		}
+	protected void asserts(List<Song> songs) {
+		Assert.assertTrue(songs.size() == 12);
+		Assert.assertEquals("Consternation", songs.get(0).getTitle());
+		Assert.assertEquals("The Itch", songs.get(11).getTitle());
 	}
 }
--- src/test/java/com/excilys/ebi/sample/jpa/query/benchmark/SimpleQueryTest.java
+++ src/test/java/com/excilys/ebi/sample/jpa/query/benchmark/SimpleQueryTest.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2010-2011 eBusiness Information, Groupe Excilys (www.excilys.com)
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -15,72 +15,34 @@
  */
 package com.excilys.ebi.sample.jpa.query.benchmark;
 
-import java.util.List;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.excilys.ebi.sample.jpa.query.benchmark.model.Song;
 
 public class SimpleQueryTest extends AbstractQueryTest {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(SimpleQueryTest.class);
-
 	@Test
 	public void testJPQL() throws InterruptedException, ExecutionException {
-
-		run(new Callable<List<Song>>() {
-			@Override
-			public List<Song> call() {
-				return jpqlRepository.getSongsByArtistName("Katatonia");
-			}
-		}, "JPQL", LOGGER);
+		asserts(jpqlRepository.getSongsByArtistName("Katatonia"));
 	}
 
 	@Test
 	public void testQueryDsl() throws InterruptedException, ExecutionException {
-
-		run(new Callable<List<Song>>() {
-			@Override
-			public List<Song> call() {
-				return queryDslRepository.getSongsByArtistName("Katatonia");
-			}
-		}, "QueryDsl", LOGGER);
+		asserts(queryDslRepository.getSongsByArtistName("Katatonia"));
 	}
 
 	@Test
 	public void testNamedQuery() throws InterruptedException, ExecutionException {
-
-		run(new Callable<List<Song>>() {
-			@Override
-			public List<Song> call() {
-				return namedQueryRepository.getSongsByArtistName("Katatonia");
-			}
-		}, "Named Query", LOGGER);
+		asserts(namedQueryRepository.getSongsByArtistName("Katatonia"));
 	}
 
 	@Test
 	public void testJPACriteria() throws InterruptedException, ExecutionException {
-
-		run(new Callable<List<Song>>() {
-			@Override
-			public List<Song> call() {
-				return jpaCriteriaRepository.getSongsByArtistName("Katatonia");
-			}
-		}, "JPA Criteria", LOGGER);
+		asserts(jpaCriteriaRepository.getSongsByArtistName("Katatonia"));
 	}
 
 	@Test
 	public void testSpringDataRepository() throws InterruptedException, ExecutionException {
-
-		run(new Callable<List<Song>>() {
-			@Override
-			public List<Song> call() {
-				return springDataRepository.getSongsByArtistName("Katatonia");
-			}
-		}, "Spring Data JPA", LOGGER);
+		asserts(springDataRepository.getSongsByArtistName("Katatonia"));
 	}
 }
