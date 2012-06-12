04b1b30afd356a99ede86fdff90dddc0f7444c50
Stephane Landelle <slandelle+github@excilys.com>
2012-06-12 22:46:46 +0200

    Use lombok

--- src/main/java/com/excilys/ebi/sample/jpa/query/benchmark/model/Artist.java
+++ src/main/java/com/excilys/ebi/sample/jpa/query/benchmark/model/Artist.java
@@ -22,63 +22,29 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "name")
 public class Artist {
 
+	@Id
 	private Integer id;
 
 	private String name;
 
+	@OneToMany(mappedBy = "artist")
 	private Set<Song> songs = new HashSet<Song>();
 
-	@Id
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@OneToMany(mappedBy = "artist")
 	public Set<Song> getSongs() {
 		return songs;
 	}
 
 	public void setSongs(Set<Song> songs) {
 		this.songs = songs;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Artist other = (Artist) obj;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		return true;
 	}
 }
--- src/main/java/com/excilys/ebi/sample/jpa/query/benchmark/model/Song.java
+++ src/main/java/com/excilys/ebi/sample/jpa/query/benchmark/model/Song.java
@@ -22,72 +22,25 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 @NamedQueries({/**/
 @NamedQuery(name = "getSongsByArtistName", query = "from Song where artist.name=? order by title"),/**/
 @NamedQuery(name = "getSongsBySameArtist", query = "select s2 from Song s1 join s1.artist.songs s2 where s1.id=? order by s2.title") })
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "title", "artist" })
 public class Song {
 
+	@Id
 	private Integer id;
 
 	private String title;
 
+	@ManyToOne(fetch = FetchType.LAZY)
 	private Artist artist;
 
-	@Id
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	public Artist getArtist() {
-		return artist;
-	}
-
-	public void setArtist(Artist artist) {
-		this.artist = artist;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((artist == null) ? 0 : artist.getId().hashCode());
-		result = prime * result + ((title == null) ? 0 : title.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Song other = (Song) obj;
-		if (artist == null) {
-			if (other.artist != null)
-				return false;
-		} else if (!artist.getId().equals(other.artist.getId()))
-			return false;
-		if (title == null) {
-			if (other.title != null)
-				return false;
-		} else if (!title.equals(other.title))
-			return false;
-		return true;
-	}
 }
